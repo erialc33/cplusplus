0155ee2fb862f76a629ff842f887d02c5c578914
erialc33 <44615630+erialc33@users.noreply.github.com>
2019-06-08 20:36:02 +0100

    Update Ch5Ex1.cpp

--- Ch5Ex1.cpp
+++ Ch5Ex1.cpp
@@ -1,6 +1,6 @@
-*/ Chapter 5 Exercise 1 from 기본개념과핵심원리로배우는C++프로그래밍
+/** Chapter 5 Exercise 1 from 기본개념과핵심원리로배우는C++프로그래밍
 다음 프로그램의 출력 결과가 문자열 "C++"가 되도록 Set 함수를 작성하시오. 
-Write a function Set that prints string "C++"/*
+Write a function Set that prints string "C++" */
 
 #include <iostream>
 using namespace std;
