73f8f86789178297b58e6dcf112386c89803d40d
erialc33 <44615630+erialc33@users.noreply.github.com>
2019-06-08 20:33:44 +0100

    Update and rename Ch5Ex1 to Ch5Ex1.cpp

---
+++ Ch5Ex1.cpp
@@ -0,0 +1,21 @@
+*/ Chapter 5 Exercise 1 from 기본개념과핵심원리로배우는C++프로그래밍
+다음 프로그램의 출력 결과가 문자열 "C++"가 되도록 Set 함수를 작성하시오. 
+Write a function Set that prints string "C++"/*
+
+#include <iostream>
+using namespace std;
+
+void Set(char* pt)     // define function set that uses char* as parameter
+  {
+    *pt = 'C';
+    *(pt + 1) = '+';
+    *(pt + 2) = '+';
+  }
+
+int main()
+{
+  int a = 0;
+  char* s = (char*)&a;
+  Set(s);
+  cout << (char*)&a << endl;
+}
