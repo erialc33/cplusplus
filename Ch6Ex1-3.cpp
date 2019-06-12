582d0bcb60414d96093e5add84516038c163f7ce
erialc33 <44615630+erialc33@users.noreply.github.com>
2019-06-12 12:08:04 +0100

    Create Ch6Ex1-3.cpp

---
+++ Ch6Ex1-3.cpp
@@ -0,0 +1,28 @@
+//원소들을 내림차순으
+
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+  int a, b; 
+ int arr[] = {1,6,9,7,3,2,0,4,8,5};
+ for (int i = 0 ;i<10 ; i++)
+ {
+  for (int j = i ; j < 10; j++)
+  {
+    a = arr[i];
+    b= arr[j];
+    if (a<b)
+    {
+      arr[i] = b;
+      arr[j] = a;
+    }
+  }
+ }
+ for(int i = 0 ; i <10 ; i++)
+ {
+   cout<<arr[i]<<" ";
+ }
+ return 0;
+}
